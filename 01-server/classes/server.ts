e6d3c9d185f6f4fc91dcb8f3850d35d451befceb
pluces <slogicos2001@gmai.com>
2022-01-14 11:20:14 -0500

    seccion 4

--- 01-server/classes/server.ts
+++ 01-server/classes/server.ts
@@ -1,19 +1,52 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/enviroment';
+import socketIO from 'socket.io';
+import http from 'http';
+import * as socket  from '../sockets/sockets';
 
-export default  class  Server {
 
+
+export default  class  Server { 
+    private static _instance : Server;
     public app: express.Application;
     public port: number;
 
+    public io: socketIO.Server;
+    private httpServer: http.Server;
 
-    constructor() {
+
+
+    private constructor() {
         this.port = SERVER_PORT;
         this.app = express();
+
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO( this.httpServer )
+        this.escucharSockets();
+
+    }
+    public static  get instance() {
+
+        return this._instance || (this._instance = new this());
+    }
+
+    private escucharSockets() {
+        console.log('Escuchando conexiones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('Cliente conectado!!');
+
+            //Mensajes
+
+            socket.mensaje(cliente,this.io);
+
+            socket.desconectar(cliente);
+
+           
+        });
     }
 
     start() {
-        this.app.listen(this.port, () => {console.log(`Servidor corriendo en el puerto ${this.port}`)});
+        this.httpServer.listen(this.port, () => {console.log(`Servidor corriendo en el puerto ${this.port}`)});
     }
 
 }
--- 01-server/index.ts
+++ 01-server/index.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 
 
 
-const server = new Server();
+const server = Server.instance;
 
 //BodyParser
 
@@ -14,9 +14,17 @@ server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json());
 
 
-//Cors
+// Cors Para la version de socket.io 4 o superior se debe especificar el dominio, los headers y el metodo.
 
-server.app.use(cors({origin: true, credentials: true}));
+server.app.use(cors({
+    origin: "http://localhost:4200", credentials: true,
+    allowedHeaders: ["my-custom-header"],
+    methods: ["GET", "POST"]
+
+   
+    
+}));
+
 
 //Rutas de servicios
 
